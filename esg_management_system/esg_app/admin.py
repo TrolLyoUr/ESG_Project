188150b6aa3b25b864ccada18176ab0c03e8484c
sea <zbljz98@Live.com>
2024-03-12 20:13:35 +1100

    modify esg_app and create new app "auth"

--- esg_management_system/esg_app/admin.py
+++ esg_management_system/esg_app/admin.py
@@ -1,33 +1,44 @@
 from django.contrib import admin
-from .models import Location, Company, Framework, Metric, Indicator, DataValue, FrameworkMetric, MetricIndicator, UserMetricPreference, UserIndicatorPreference
+from .models import Location, Company, Framework, Metric, Indicator, DataValue, FrameworkMetric, MetricIndicator, \
+    UserMetricPreference, UserIndicatorPreference
+
 
 # Optional: Define custom admin classes to customize the admin interface
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'location')
 
+
 class FrameworkAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
+
 
 class MetricAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'pillar', 'framework')
 
+
 class IndicatorAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'source')
+
 
 class DataValueAdmin(admin.ModelAdmin):
     list_display = ('company', 'indicator', 'year', 'value')
 
+
 class FrameworkMetricAdmin(admin.ModelAdmin):
     list_display = ('framework', 'metric', 'predefined_weight')
+
 
 class MetricIndicatorAdmin(admin.ModelAdmin):
     list_display = ('metric', 'indicator', 'predefined_weight')
 
+
 class UserMetricPreferenceAdmin(admin.ModelAdmin):
     list_display = ('user', 'framework', 'metric', 'custom_weight')
 
+
 class UserIndicatorPreferenceAdmin(admin.ModelAdmin):
     list_display = ('user', 'metric', 'indicator', 'custom_weight')
+
 
 # Register your models here
 admin.site.register(Location)
