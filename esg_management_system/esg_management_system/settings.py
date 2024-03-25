ae85f6e771fe42d9d67690c76da8e162c53e3f15
sea <zbljz98@Live.com>
2024-03-25 18:04:41 +1100

    disable csrf

--- esg_management_system/esg_management_system/settings.py
+++ esg_management_system/esg_management_system/settings.py
@@ -130,3 +130,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates', 'build', 'static'), ]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_HTTPONLY = False
