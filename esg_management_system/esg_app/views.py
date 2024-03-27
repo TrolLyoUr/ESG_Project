3cccc6f5e215a8e45413ef7b07a77566c1b8a30d
sea <zbljz98@Live.com>
2024-03-27 17:35:11 +1100

    split api and front end

--- esg_management_system/esg_app/views.py
+++ esg_management_system/esg_app/views.py
@@ -1,10 +1,12 @@
 from rest_framework import permissions, viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ListUsers(viewsets.ModelViewSet):
-    authentication_classes = [permissions.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
