afc506b099b88c211bfab6f32781dfe9d18d7f87
sea <zbljz98@Live.com>
2024-03-27 18:39:32 +1100

    remove cors hosts

--- esg_management_system/esg_management_system/settings.py
+++ esg_management_system/esg_management_system/settings.py
@@ -58,6 +58,10 @@ ROOT_URLCONF = 'esg_management_system.urls'
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS Settings
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4000",
+    "http://9900.seasite.top:4000"  # Add any other domains you want to allow
+]
 
 TEMPLATES = [
     {
