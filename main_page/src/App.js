97a7300c3bd3919fd1667a94f42a40c4889fcd2a
Bernie Lee <bob870817@gmail.com>
2024-04-02 18:39:11 +1100

    Refactor serializers and views for Framework model

--- main_page/src/App.js
+++ main_page/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Sidebar';
-import Header from './Header';
-import Feature1 from './Feature1'; // 引入 Feature1 组件
-import Feature4 from './Feature4';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import Feature1 from "./Feature1"; // 引入 Feature1 组件
+import Feature4 from "./Feature4";
 
 const App = () => {
   return (
@@ -11,13 +11,11 @@ const App = () => {
       <Header />
       <Sidebar />
       <div className="main-content">
-        <h1>Welcome to Our Application</h1>
-       
         <div className="feature-areas">
-            <Feature1 /> 
-            <div className="feature-area feature-area-2">Feature 2</div>
-            <div className="feature-area feature-area-3">Feature 3</div>
-            <Feature4 /> 
+          <Feature1 />
+          <div className="feature-area feature-area-2">Feature 2</div>
+          <div className="feature-area feature-area-3">Feature 3</div>
+          <Feature4 />
         </div>
       </div>
     </div>
