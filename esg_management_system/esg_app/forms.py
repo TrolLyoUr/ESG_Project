34227b9f8fc740bf66e9dea5e6ee783a209c3f54
Jingchen Xu <z5471272@ad.unsw.edu.au>
2024-04-02 16:56:00 +0800

    temp calculation_form

--- esg_management_system/esg_app/forms.py
+++ esg_management_system/esg_app/forms.py
@@ -3,4 +3,21 @@
 from django import forms
 
 class CalculationForm(forms.Form):
+    metrics = forms.JSONField()
+
+    def calculate(self):
+        metrics = self.cleaned_data.get('metrics')
+        new_sum = 0
+        
+        for metric in metrics:
+            metric_id = metric.get('metric_id')
+            custom_weight = metric.get('custom_weight')
+
+            metric_value = get_metric_value(metric_id)
+            new_sum += metric_value * custom_weight
+
+        return new_sum
+
+
+class CustomWeightSaveForm(forms.Form):
     pass
