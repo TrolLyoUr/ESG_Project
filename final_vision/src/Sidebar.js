6d45d83beeb31d11242489285551f178c0e4b8a3
panzerkpzw <49144009+panzerkpzw@users.noreply.github.com>
2024-04-16 15:42:51 +1000

    Update Sidebar.js
    year and the company can be shown by backend

--- final_vision/src/Sidebar.js
+++ final_vision/src/Sidebar.js
@@ -1,76 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import "./Sidebar.css";
 import axios from "axios";
-import { SERVER_URL } from "./config";
 
-const serverUrl = SERVER_URL;
 axios.defaults.withCredentials = true;
 
-const Sidebar = ({ isOpen, setCompanyId, setYear }) => {
+const Sidebar = ({ isOpen }) => {
   const sidebarClasses = `sidebar ${isOpen ? "open" : "hidden"}`;
   const [selectedCompany, setSelectedCompany] = useState("");
   const [filteredCompanies, setFilteredCompanies] = useState([]);
-  const [years, setYears] = useState([]); // State for storing the list of years
   const [selectedYear, setSelectedYear] = useState("");
+  const [filteredYears, setFilteredYears] = useState([]);
   const [isComparing, setIsComparing] = useState(false);
   const [selectedCompany2, setSelectedCompany2] = useState("");
   const [selectedYear2, setSelectedYear2] = useState("");
 
-  useEffect(() => {
-    const fetchYears = async () => {
-      try {
-        const response = await axios.get(`${serverUrl}/app/years/`);
-        setYears(response.data.map((item) => item.year));
-      } catch (error) {
-        console.error("Failed to fetch years:", error);
-      }
-    };
-
-    fetchYears();
-  }, []);
-
-  const handleInputChange = async (e, isSecondCompany = false) => {
+  const handleCompanyChange = async (e, isSecondCompany = false) => {
     const value = e.target.value.trim();
     if (!isSecondCompany) {
       setSelectedCompany(value);
     } else {
       setSelectedCompany2(value);
     }
-
     if (value.length >= 1) {
       try {
-        const apiURL = `${serverUrl}/app/fsearch/${value}/`;
-        const response = await axios.get(apiURL, { withCredentials: true });
-        const companies = response.data.map((company) => ({
-          name: company.name,
-          id: company.id,
-        }));
-        if (!isSecondCompany) {
-          setFilteredCompanies(companies);
-        } else {
-          // Handle second company filtered list similarly if required
-        }
+          // 根据用户输入动态构造API URL
+          const apiURL = `http://9900.seasite.top:8000/app/fsearch/${value}/`;
+          const response = await axios.get(apiURL, { withCredentials: true });
+
+          // 提取公司名称并更新状态
+        const companyName = response.data.map(company => company.name);
+          setFilteredCompanies(companyName);
       } catch (error) {
-        console.error("Failed to fetch filtered companies:", error);
+          console.error('Failed to fetch filtered companies:', error);
       }
     } else {
       if (!isSecondCompany) {
         setFilteredCompanies([]);
-      } else {
-        // Clear second company's filtered companies if needed
+        } else {
+            // Clear second company's filtered companies if needed
+      } 
+    }
+  };
+
+    useEffect(() => {
+    const fetchYears = async () => {
+      try {
+        const apiURL = `http://9900.seasite.top:8000/app/years/`; // Ensure this is the correct API endpoint
+        const response = await axios.get(apiURL, { withCredentials: true });
+        const years = response.data.map(yearItem => yearItem.year);
+        setFilteredYears(years);
+      } catch (error) {
+        console.error('Failed to fetch years:', error);
       }
-    }
-  };
+    };
 
-  const finalizeSelection = () => {
-    const company = filteredCompanies.find((c) => c.name === selectedCompany);
-    if (company) {
-      setCompanyId(company.id);
-    }
-  };
+    fetchYears();
+    }, []);
 
+  const handleYearChange = async (e, isSecondYear = false) => {
+    const value = e.target.value.trim();
+    if (!isSecondYear) {
+      setSelectedYear(value);
+    } else {
+      setSelectedYear2(value);
+    }
+ 
+  };
   const toggleCompare = () => setIsComparing(!isComparing);
+/*
+  const handleSubmit = useCallback(async () => {
+    if (selectedYear) {
+        const dataToSend = { // Assuming selectedCompany holds the ID
+            year: selectedYear
+        };
 
+        try {
+            const response = await axios.post('http://9900.seasite.top:8000/app/submitData', dataToSend, { withCredentials: true });
+            console.log('Data submitted successfully:', response.data);
+        } catch (error) {
+            console.error('Failed to submit data:', error);
+        }
+    }
+}, [selectedYear]);
+
+    useEffect(() => {
+    if (selectedYear) {
+        handleSubmit();
+        }
+    }, [selectedYear, handleSubmit]);
+*/
   return (
     <div className={sidebarClasses}>
       <div className="sidebar-header">
@@ -91,76 +109,52 @@ const Sidebar = ({ isOpen, setCompanyId, setYear }) => {
               list="company-options"
               id="company"
               value={selectedCompany}
-              onChange={(e) => handleInputChange(e)}
-              onBlur={finalizeSelection}
+              onChange={(e) => handleCompanyChange(e)}
               placeholder="Search a company"
             />
             <datalist id="company-options">
               {filteredCompanies.map((company, index) => (
-                <option key={index} value={company.name} />
+                <option key={index} value={company} />
               ))}
             </datalist>
           </li>
           <li>
-            <label className="label" htmlFor="year">
-              Year
-            </label>
-            <select
-              className="select"
-              id="year"
-              value={selectedYear}
-              onChange={(e) => {
-                setSelectedYear(e.target.value);
-                setYear(e.target.value);
-              }}
-            >
-              <option value="" disabled hidden>
-                Choose a year
-              </option>
-              {years.map((year) => (
-                <option key={year} value={year}>
-                  {year}
-                </option>
+            <label className="label" htmlFor="year">Year</label>
+            <select className="select" id="year" value={selectedYear} onChange={e => handleYearChange(e)}>
+              <option value="" disabled hidden>Choose a year</option>
+              {filteredYears.map((year, index) => (
+                <option key={index} value={year}>{year}</option>
               ))}
             </select>
           </li>
           <li>
-            <button className="compare-btn" onClick={toggleCompare}>
-              Compare with another company
-            </button>
+            <button className="compare-btn" onClick={toggleCompare}>Compare with another company</button>
           </li>
           {isComparing && (
             <>
               <li>
-                <label className="label" htmlFor="company2">
-                  Second Company
-                </label>
+                <label className="label" htmlFor="company2">Second Company</label>
                 <input
                   className="input"
                   list="company-options2"
                   id="company2"
                   value={selectedCompany2}
-                  onChange={(e) => handleInputChange(e, true)}
+                  onChange={(e) => handleCompanyChange(e, true)}
                   placeholder="Search another company"
                 />
+                {/* You might need a separate datalist for the second company */}
+                <datalist id="company-options2">
+                  {filteredCompanies.map((company, index) => (
+                    <option key={index} value={company} />
+                  ))}
+                </datalist>
               </li>
               <li>
-                <label className="label" htmlFor="year2">
-                  Year
-                </label>
-                <select
-                  className="select"
-                  id="year2"
-                  value={selectedYear2}
-                  onChange={(e) => setSelectedYear2(e.target.value)}
-                >
-                  <option value="" disabled hidden>
-                    Choose a year
-                  </option>
-                  {years.map((year) => (
-                    <option key={year} value={year}>
-                      {year}
-                    </option>
+                <label className="label" htmlFor="year2">Year</label>
+                <select className="select" id="year2" value={selectedYear2} onChange={e => handleYearChange(e, true)}>
+                  <option value="" disabled hidden>Choose a year</option>
+                  {filteredYears.map((year, index) => (
+                    <option key={index} value={year}>{year}</option>
                   ))}
                 </select>
               </li>
