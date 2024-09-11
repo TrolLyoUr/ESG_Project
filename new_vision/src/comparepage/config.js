942b3929be27fd2f7a801b97395f46c6a3dbb8cf
Bernie Lee <z5351857>
2024-09-11 20:11:42 +1000

    Modify api url in front-end and integrated in Django app

--- new_vision/src/comparepage/config.js
+++ new_vision/src/comparepage/config.js
@@ -1,2 +1,3 @@
-const BACKEND_PORT = 8000;
-export const SERVER_URL = "http://9900.seasite.top:" + BACKEND_PORT;
+// const BACKEND_PORT = 8000;
+// export const SERVER_URL = "http://9900.seasite.top:" + BACKEND_PORT;
+export const SERVER_URL = "";
